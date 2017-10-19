937d7125740dc9e7e5f29b50936c45fd225d325b
Joe Bowser <bowserj@apache.org>
2017-10-19 14:20:43 -0700

    eslint fix

--- src/ConfigChanges/ConfigFile.js
+++ src/ConfigChanges/ConfigFile.js
@@ -195,8 +195,7 @@ function resolveConfigFilePath (project_dir, platform, file) {
         } else if (file.endsWith('strings.xml')) {
             // Plugins really shouldn't mess with strings.xml, since it's able to be localized
             filepath = path.join(project_dir, 'app', 'src', 'main', 'res', 'values', 'strings.xml');
-        }
-        else if (file.match(/res\/xml/)) {
+        } else if (file.match(/res\/xml/)) {
             // Catch-all for all other stored XML configuration in legacy plugins
             var config_file = path.basename(file);
             filepath = path.join(project_dir, 'app', 'src', 'main', 'res', 'xml', config_file);
