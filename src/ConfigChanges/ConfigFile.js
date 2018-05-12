393cda1d7baec50819c77ae0207ba5622d36c72b
Darryl Pogue <darryl@dpogue.ca>
2018-05-12 00:22:21 -0700

    CB-11691: Fix for modifying binary plists
    The bplist-parser module returns the plist dictionary object wrapped in
    an array. When making modifications with `<edit-config>` or
    `<config-file>`, it would add the changes as properties to the array but
    then ignore them when writing out to the filesystem.

--- src/ConfigChanges/ConfigFile.js
+++ src/ConfigChanges/ConfigFile.js
@@ -82,7 +82,7 @@ function ConfigFile_load () {
         //       Do we still need to support binary plist?
         //       If yes, use plist.parseStringSync() and read the file once.
         self.data = isBinaryPlist(filepath) ?
-            modules.bplist.parseBuffer(fs.readFileSync(filepath)) :
+            modules.bplist.parseBuffer(fs.readFileSync(filepath))[0] :
             modules.plist.parse(fs.readFileSync(filepath, 'utf8'));
     }
 }
