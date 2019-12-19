06c995b427f6fe7a2c617ebe90a93005b3a46dc8
Raphael von der Grün <raphinesse@gmail.com>
2019-12-19 18:39:05 +0100

    refactor process_munge to processArgs & getElements
    We simplify getElements since it's always called with a top-level munge

--- src/ConfigChanges/munge-util.js
+++ src/ConfigChanges/munge-util.js
@@ -18,89 +18,68 @@ const _ = require('underscore');
 
 // add the count of [key1][key2]...[keyN] to obj
 // return true if it didn't exist before
-exports.deep_add = function deep_add (obj, keys /* or key1, key2 .... */) {
-    if (!Array.isArray(keys)) {
-        keys = Array.prototype.slice.call(arguments, 1);
+exports.deep_add = (...args) => {
+    const { element: k, siblings: parentArray } = processArgs(...args, { create: true });
+    const found = _.find(parentArray, element => element.xml === k.xml);
+
+    if (found) {
+        found.after = found.after || k.after;
+        found.count += k.count;
+    } else {
+        parentArray.push(k);
     }
 
-    return process_munge(obj, true/* createParents */, (parentArray, k) => {
-        const found = _.find(parentArray, element => element.xml === k.xml);
-
-        if (found) {
-            found.after = found.after || k.after;
-            found.count += k.count;
-        } else {
-            parentArray.push(k);
-        }
-
-        return !found;
-    }, keys);
+    return !found;
 };
 
 // decrement the count of [key1][key2]...[keyN] from obj and remove if it reaches 0
 // return true if it was removed or not found
-exports.deep_remove = function deep_remove (obj, keys /* or key1, key2 .... */) {
-    if (!Array.isArray(keys)) {
-        keys = Array.prototype.slice.call(arguments, 1);
+exports.deep_remove = (...args) => {
+    const { element: k, siblings: parentArray } = processArgs(...args);
+    const index = _.findIndex(parentArray, element => element.xml === k.xml);
+
+    if (index < 0) return true;
+
+    const found = parentArray[index];
+
+    if (found.oldAttrib) {
+        k.oldAttrib = _.extend({}, found.oldAttrib);
     }
+    found.count -= k.count;
 
-    return process_munge(obj, false/* createParents */, (parentArray, k) => {
-        const index = _.findIndex(parentArray, element => element.xml === k.xml);
+    if (found.count > 0) return false;
 
-        if (index < 0) return true;
-
-        const found = parentArray[index];
-
-        if (found.oldAttrib) {
-            k.oldAttrib = _.extend({}, found.oldAttrib);
-        }
-        found.count -= k.count;
-
-        if (found.count > 0) return false;
-
-        parentArray.splice(index, 1);
-        return true;
-    }, keys);
+    parentArray.splice(index, 1);
+    return true;
 };
 
 // search for [key1][key2]...[keyN]
 // return the object or undefined if not found
-exports.deep_find = function deep_find (obj, keys /* or key1, key2 .... */) {
-    if (!Array.isArray(keys)) {
-        keys = Array.prototype.slice.call(arguments, 1);
-    }
+exports.deep_find = (...args) => {
+    const { element: k, siblings: parentArray } = processArgs(...args);
 
-    return process_munge(obj, false/* createParents? */, (parentArray, k) => {
-        return _.find(parentArray, element => element.xml === (k.xml || k));
-    }, keys);
+    return _.find(parentArray, element => element.xml === (k.xml || k));
 };
 
-// Execute func passing it the parent array and the xmlChild key.
-// When createParents is true, add the file and parent items  they are missing
-// When createParents is false, stop and return undefined if the file and/or parent items are missing
+function processArgs (obj, fileName, selector, element, opts) {
+    if (Array.isArray(fileName)) {
+        opts = selector;
+        [fileName, selector, element] = fileName;
+    }
 
-function process_munge (obj, createParents, func, keys /* or key1, key2 .... */) {
-    if (!Array.isArray(keys)) {
-        keys = Array.prototype.slice.call(arguments, 1);
-    }
-    const k = keys[0];
-    if (keys.length === 1) {
-        return func(obj, k);
-    } else if (keys.length === 2) {
-        if (!obj.parents[k] && !createParents) {
-            return undefined;
-        }
-        obj.parents[k] = obj.parents[k] || [];
-        return process_munge(obj.parents[k], createParents, func, keys.slice(1));
-    } else if (keys.length === 3) {
-        if (!obj.files[k] && !createParents) {
-            return undefined;
-        }
-        obj.files[k] = obj.files[k] || { parents: {} };
-        return process_munge(obj.files[k], createParents, func, keys.slice(1));
-    } else {
-        throw new Error('Invalid key format. Must contain at most 3 elements (file, parent, xmlChild).');
-    }
+    const siblings = getElements(obj, [fileName, selector], opts);
+    return { element, siblings };
+}
+
+// Get the element array for given keys
+// If a key entry is missing, create it if opts.create is true else return []
+function getElements ({ files }, [fileName, selector], opts = { create: false }) {
+    if (!files[fileName] && !opts.create) return [];
+
+    const { parents: fileChanges } = (files[fileName] = files[fileName] || { parents: {} });
+    if (!fileChanges[selector] && !opts.create) return [];
+
+    return (fileChanges[selector] = fileChanges[selector] || []);
 }
 
 // All values from munge are added to base as
