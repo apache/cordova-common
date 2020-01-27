6120b865023330c3332553dc0b1d75ce23300ded
Arman Sargsyan <mail@arman.io>
2020-01-27 17:10:53 +0300

    fix(ConfigParser): correct resolve *-Info.plist file path

--- src/ConfigChanges/ConfigFile.js
+++ src/ConfigChanges/ConfigFile.js
@@ -174,8 +174,9 @@ function resolveConfigFilePath (project_dir, platform, file) {
 
         // [CB-5989] multiple Info.plist files may exist. default to $PROJECT_NAME-Info.plist
         if (matches.length > 1 && file.includes('-Info.plist')) {
-            const plistName = `/${getIOSProjectname(project_dir)}-Info.plist`;
-            filepath = matches.find(m => m.includes(plistName)) || filepath;
+            const projName = getIOSProjectname(project_dir);
+            const plistName = projName + '-Info.plist';
+            return matches.find(m => m === path.join(project_dir, projName, plistName)) || filepath;
         }
         return filepath;
     }
